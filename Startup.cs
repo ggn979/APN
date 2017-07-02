2184885d3d14de82e3e6ff057df672b09d7b2a35
ggn979 <ggn979@gmail.com>
2017-07-01 23:48:37 -0400

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(G_APM.Startup))]
+
+namespace G_APM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
